8aed58b5af92c07f0783907f93e105783fe86fc4
Quangsonz <lihgtningzz21@gmail.com>
2025-02-19 21:47:16 +0700

    hahaha

---
+++ src/components/NavItems.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const NavItems = () => {
+
+    const [menuToggle, setMenuToggle] = useState(false);
+    const [socialToggle, setsocialToggle] = useState(false);
+    const [headerFixed, setHeaderFixed] = useState(false);
+  return (
+    <div>NavItems</div>
+  )
+}
+
+export default NavItems
